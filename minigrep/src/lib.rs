8548a6a9f190fe5bb4a36faf253cf195f10c14c3
Bryce Stabenow <brycestabenow617@gmail.com>
2024-01-25 22:10:41 -0600

    end chapter 12

--- minigrep/src/lib.rs
+++ minigrep/src/lib.rs
@@ -1,18 +1,58 @@
 use std::error::Error;
 use std::fs;
+use std::env;
+
 
 pub fn run(args: Args) -> Result<(), Box<dyn Error>>{
     let file_contents: String = fs::read_to_string(args.file_path)?;
 
-    println!("{}", file_contents);
+    let results: Vec<&str> = match args.case_insensitive{
+        true => search_case_insensitive(&args.query, &file_contents),
+        false => search(&args.query, &file_contents),
+    };
+
+    if results.len() > 0 {
+        for line in results{
+            println!("{line}");
+        };
+    } else {
+        println!("No matches found");
+    }
 
     Ok(())
 }
 
 
+pub fn search<'a>(query: &str, contents: &'a str) -> Vec<&'a str> {
+    let mut results = Vec::new();
+
+    for line in contents.lines() {
+        if line.contains(query){
+            results.push(line);
+        }
+    }
+
+    results
+}
+
+pub fn search_case_insensitive<'a>(query: &str, contents: &'a str) -> Vec<&'a str> {
+    let mut results = Vec::new();
+    let query = query.to_lowercase();
+
+    for line in contents.lines() {
+        if line.to_lowercase().contains(&query){
+            results.push(line);
+        }
+    }
+
+    results
+}
+
+
 pub struct Args {
     query: String,
-    file_path: String
+    file_path: String,
+    case_insensitive: bool,
 }
 
 
@@ -24,7 +64,41 @@ impl Args {
 
         Ok(Args {
             query: args[1].clone(),
-            file_path: args[2].clone()
+            file_path: args[2].clone(),
+            case_insensitive: env::var("IGNORE_CASE").is_ok(),
         })
+    }
+}
+
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn case_sensitive() {
+        let query = "duct";
+        let contents = "\
+Rust:
+safe, fast, productive.
+Pick three.
+Duct tape.";
+
+        assert_eq!(vec!["safe, fast, productive."], search(query, contents));
+    }
+
+    #[test]
+    fn case_insensitive() {
+        let query = "rUsT";
+        let contents = "\
+Rust:
+safe, fast, productive.
+Pick three.
+Trust me.";
+
+        assert_eq!(
+            vec!["Rust:", "Trust me."],
+            search_case_insensitive(query, contents)
+        );
     }
 }
--- minigrep/src/main.rs
+++ minigrep/src/main.rs
@@ -8,12 +8,12 @@ fn main() {
     
     let args: Args = Args::build(&args)
         .unwrap_or_else(|err| {
-            println!("{}", err);
+            eprintln!("Parsing error: {err}");
             process::exit(1);
         });
 
     if let Err(e) = minigrep::run(args) {
-        println!("{}", e);
+        eprintln!("Program error: {e}");
         process::exit(1);
     };
 }
