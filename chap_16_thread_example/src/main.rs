33dcdafca87edd34dcc1bd1d033c0856b13e2f02
Bryce Stabenow <brycestabenow617@gmail.com>
2024-02-11 12:43:28 -0600

    play with sleep duration

--- chap_16_thread_example/src/main.rs
+++ chap_16_thread_example/src/main.rs
@@ -1,18 +1,20 @@
-use std::{sync::mpsc, thread};
+use std::{sync::mpsc, thread, time::Duration};
 
 fn main() {
     let (tx, rx) = mpsc::channel();
     let tx2: mpsc::Sender<i32> = tx.clone();
 
     thread::spawn( move || {
-        for i in 1..=1000 {
+        for i in 1..=10000 {
             tx.send(i).unwrap();
+            thread::sleep(Duration::from_nanos(1));
         }
     });
 
     thread::spawn( move || {
-        for i in 1..=500 {
+        for i in 1..=5000 {
             tx2.send(i).unwrap();
+            thread::sleep(Duration::from_nanos(1));
         }
     });
 
