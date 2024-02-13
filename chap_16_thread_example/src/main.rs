bc7dfa71a3bbc9453b0c86b55a776f7486c04bb6
Bryce Stabenow <brycestabenow617@gmail.com>
2024-02-12 20:54:28 -0600

    threads and tx rx fun

--- chap_16_thread_example/src/main.rs
+++ chap_16_thread_example/src/main.rs
@@ -1,39 +1,52 @@
-use std::{sync::{Arc, Mutex}, thread};
+use std::{sync::{mpsc, Arc, Mutex}, thread};
 
 fn main() {
     let count: Arc<Mutex<i32>> = Arc::new(Mutex::new(0));
     let mut handles = vec![];
     
-    for _ in 0..50 {    
-        let count1 = count.clone();
+    let (tx, rx) = mpsc::channel::<String>();
+    
+    for i in 0..500 {    
+        let tx = tx.clone();
+        let count1 = Arc::clone(&count);
         let handle = thread::spawn(move || {
             let mut arr = count1.lock().unwrap();
             *arr += 1;
-            println!("Thread 1 - Count: {}", arr);
+            tx.send(format!("Thread {}: {}", i, arr))
         });
 
         handles.push(handle);
     }
 
-    for _ in 0..50 {
+    for i in 0..500 {
+        let tx = tx.clone();
         let count2 = Arc::clone(&count);
         let handle = thread::spawn(move || {
             let mut arr = count2.lock().unwrap();
             *arr += 1;
-            println!("Thread 2 - Count: {}", arr);
+            tx.send(format!("Thread {}: {}", i, arr))
         });
     
         handles.push(handle);
     }
 
-    for _ in 0..50 {
+    for _ in 0..500 {
         let mut arr = count.lock().unwrap();
         *arr += 1;
 
-        println!("Main - Count: {}", arr);
+        tx.send(format!("Main: {}", arr)).unwrap();
     }
 
+    drop(tx);
+
     for handle in handles {
-        handle.join().unwrap();
+        match handle.join() {
+            Err(err) => println!("{:?}", err),
+            _ => {}
+        }
+    }
+
+    for message in rx {
+        println!("{:?}", message);
     }
 }
