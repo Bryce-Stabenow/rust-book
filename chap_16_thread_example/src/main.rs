ff5fda659dd30e30360fe3f4c543efa1a992d5df
Bryce Stabenow <brycestabenow617@gmail.com>
2024-02-12 20:34:58 -0600

    more threading practice

--- chap_16_thread_example/src/main.rs
+++ chap_16_thread_example/src/main.rs
@@ -2,22 +2,38 @@ use std::{sync::{Arc, Mutex}, thread};
 
 fn main() {
     let count: Arc<Mutex<i32>> = Arc::new(Mutex::new(0));
+    let mut handles = vec![];
     
-    for _ in 0..100 {    
+    for _ in 0..50 {    
         let count1 = count.clone();
-        thread::spawn(move || {
-                let mut arr = count1.lock().unwrap();
-                *arr += 1
-            });
+        let handle = thread::spawn(move || {
+            let mut arr = count1.lock().unwrap();
+            *arr += 1;
+            println!("Thread 1 - Count: {}", arr);
+        });
+
+        handles.push(handle);
     }
 
     for _ in 0..50 {
-    let count2 = Arc::clone(&count);
-    thread::spawn(move || {
+        let count2 = Arc::clone(&count);
+        let handle = thread::spawn(move || {
             let mut arr = count2.lock().unwrap();
             *arr += 1;
+            println!("Thread 2 - Count: {}", arr);
         });
-    }
     
-    println!("Count of {}", count.lock().unwrap());
+        handles.push(handle);
+    }
+
+    for _ in 0..50 {
+        let mut arr = count.lock().unwrap();
+        *arr += 1;
+
+        println!("Main - Count: {}", arr);
+    }
+
+    for handle in handles {
+        handle.join().unwrap();
+    }
 }
