f13a6564dc9a9f5c6e6bb10d3d974568ac0e3bc1
Bryce Stabenow <brycestabenow617@gmail.com>
2024-03-02 10:01:28 -0600

    original solution

--- utilities/src/main.rs
+++ utilities/src/main.rs
@@ -11,14 +11,15 @@ fn get_drink_by_profession(param: &str) -> &'static str {
 }
 
 fn warn_the_sheep(queue: &[&str]) -> String {
-    let wolf_location = queue.rev().iter().position(|&x| x == "wolf");
+    let wolf_location = queue.iter().position(|&x| x == "wolf");
+    let length = queue.len();
     
     match wolf_location {
         Some(num) => {
-            if num == 1{
+            if num == length - 1{
                 "Pls go away and stop eating my sheep".to_string()
             } else {
-                format!("Oi! Sheep number {}! You are about to be eaten by a wolf!", num)
+                format!("Oi! Sheep number {}! You are about to be eaten by a wolf!", length - num - 1)
             }
         },
         None => "Error".to_string(),
