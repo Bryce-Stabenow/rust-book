6b48e113dcc8a0a4266cbe3d7c8ca17f3a28314c
Bryce Stabenow <brycestabenow617@gmail.com>
2024-03-02 10:00:00 -0600

    new Kata

--- utilities/src/main.rs
+++ utilities/src/main.rs
@@ -1,6 +1,5 @@
 fn get_drink_by_profession(param: &str) -> &'static str {
-    let lc = &param.to_lowercase()[..];
-    return match lc {
+    match param.to_lowercase().as_str() {
         "jabroni" => "Patron Tequila",
         "school counselor" => "Anything with Alcohol",
         "programmer" => "Hipster Craft Beer",
@@ -9,4 +8,19 @@ fn get_drink_by_profession(param: &str) -> &'static str {
         "rapper" => "Cristal",
         _ => "Beer"
     };
+}
+
+fn warn_the_sheep(queue: &[&str]) -> String {
+    let wolf_location = queue.rev().iter().position(|&x| x == "wolf");
+    
+    match wolf_location {
+        Some(num) => {
+            if num == 1{
+                "Pls go away and stop eating my sheep".to_string()
+            } else {
+                format!("Oi! Sheep number {}! You are about to be eaten by a wolf!", num)
+            }
+        },
+        None => "Error".to_string(),
+    }
 }
