3949ecef2fbbbb1af99e561ee9eb7b074b7ed004
Bryce Stabenow <brycestabenow617@gmail.com>
2024-03-02 09:41:50 -0600

    Code kata 1

--- utilities/src/main.rs
+++ utilities/src/main.rs
@@ -1,1 +1,12 @@
-
+fn get_drink_by_profession(param: &str) -> &'static str {
+    let lc = &param.to_lowercase()[..];
+    return match lc {
+        "jabroni" => "Patron Tequila",
+        "school counselor" => "Anything with Alcohol",
+        "programmer" => "Hipster Craft Beer",
+        "bike gang member" => "Moonshine",
+        "politician" => "Your tax dollars",
+        "rapper" => "Cristal",
+        _ => "Beer"
+    };
+}
